246651d4ca459b8ccafe81030483d05bfe43e0b4
Pedro.Wang <paynemax@163.com>
2020-09-21 09:27:52 +0000

    update proxypool/getter.py

--- proxypool/getter.py
+++ proxypool/getter.py
@@ -11,11 +11,10 @@ class Getter:
     """
 
     def __init__(self):
-        self.redis = RedisClient(host='127.0.0.1')
-        # TODO
-        # self.redis = RedisClient()
+        self.redis = RedisClient()
         self.crawler = Crawler()
         self.logger = logging.getLogger('main.getter')
+        self.proxy_list = []
 
 
     def collect(self):
@@ -23,7 +22,6 @@ class Getter:
         Collect proxies into a list using the functions from self.crawler.
         return: a list of proxies
         """
-        ans = []
         for callback_label in range(self.crawler.__CrawlFuncCount__):
             try:
                 count = 0
@@ -34,27 +32,27 @@ class Getter:
                 self.logger.exception(str(e.args))
             else:
                 count += len(proxies)
+                self.proxy_list.extend(proxies)
                 self.logger.info(f'Got {count} proxies with function {callback_label}')
-                ans.extend(proxies)
-        return ans
 
 
-    def run(self):
+    def store(self, redis_key=REDIS_KEY):
         """
-        Kick off collecting and put them into redis
+        Put proxies into redis
         """
         if not self.redis.is_over_threshold() and self.redis.acquire_lock():
-            ret = self.collect()
-            # TODO
-            print(ret)
-            self.redis.add_proxies(ret)
+            saved = self.redis.add_proxies(self.proxy_list, key=redis_key)
             self.redis.release_lock()
+            return saved
         else:
             self.logger.info("Failed to add collected proxies into database due to either is_over_threshold or failed to acquire_lock")
             self.redis.release_lock()
+            return 0
 
+    def run(self):
+        """
+        Start collecting and then store result into redis
+        """
+        self.collect()
+        self.store()
 
-if __name__ == "__main__":
-    getter = Getter()
-    getter.run()
-    print('xxxxxxx')
